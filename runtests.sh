96e0206d53edd42bdb302d18e6761feb999dcf5c
Hugh Saunders <hugh@wherenow.org>
2018-01-24 10:09:23 +0000

    Add shell and travis linting

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -xe
+if which travis; then
+    travis lint -x .travis.yml
+else
+    echo "Skipping travis lint as travis command not available. "
+    echo "On osx, install from brew, otherwise gem."
+fi
+bashate *.sh
 pylama --skip "*venv/*"
 pytest test_runjenkins.py
